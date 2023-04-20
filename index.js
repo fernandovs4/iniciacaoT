dd833b66d7d9be8c4584d573187c326fc9294954
fernandovs4 <fervsantos3000@gmail.com>
2023-04-20 03:18:09 -0300

    politica cors

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ let hospitaisSelecionados= [];
                 const requestOptions = {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
-                    mode: 'no-cors', 
+                    
                     body: json
                     };
 
